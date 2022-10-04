846009a0f3cf2dc6981edac8dae5f3a99c66c8ed
Edgarc510 <Voltaire0709>
2022-10-03 17:43:57 -0700

    completed create account

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -1,11 +1,17 @@
 package com.bank.controller;
 
 
+import com.bank.enums.AccountType;
+import com.bank.model.Account;
 import com.bank.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping
@@ -27,6 +33,22 @@ public class AccountController {
         model.addAttribute("accountList", accountService.listAllAccount() );
 
         return "account/index";
+    }
+    @GetMapping("/create-form")
+    public String createAccount(Model model){
+
+//        model.addAttribute("account", new Account());
+        model.addAttribute("account", Account.builder().build());
+        model.addAttribute("accountTypes", AccountType.values());
+
+        return "account/create-account";
+    }
+    @PostMapping("create")
+    public String createAccount(@ModelAttribute("account") Account account){
+
+        accountService.createNewAccount
+                (account.getBalance(),new Date(),account.getAccountType(),account.getUserID());
+        return "redirect:/index";
     }
 
 
